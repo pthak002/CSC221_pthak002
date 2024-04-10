7ba053fb005bdfa4fe29169175a41c10f0b3d8f9
pthak002 <158799697+pthak002@users.noreply.github.com>
2024-04-09 23:13:27 -0400

    Update settings.py

--- Project 1/settings.py
+++ Project 1/settings.py
@@ -6,8 +6,8 @@ class Settings:
     
         self.screen_width = 1200
         self.screen_height = 800
-        #self.bg_color = (230, 230, 230)
-        self.bg_color = (0, 0, 255)
+        self.bg_color = (230, 230, 230)
+        #self.bg_color = (0, 0, 255)
 
         # Ship 
         self.ship_speed = 1.5
