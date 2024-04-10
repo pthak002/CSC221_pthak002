13570135a7bcfb0f0d73a001f3866d93c4caa8a4
pthak002 <158799697+pthak002@users.noreply.github.com>
2024-04-09 23:10:34 -0400

    Add files via upload

---
+++ Project 1/settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+    
+        self.screen_width = 1200
+        self.screen_height = 800
+        #self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 255)
+
+        # Ship 
+        self.ship_speed = 1.5
+
+        # Bullet
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 1000
